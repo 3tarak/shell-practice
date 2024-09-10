aded79561a0e2eae65569c27f3cd359826e6c3fa
TarakPalati <tarakpalati@gmail.com>
2024-09-10 17:15:48 +0530

    I wrote a conversation script.

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "Ram: Hi Sam, how are you"
+echo "Sam: Hi Ram, I am doing good. How about you Ram?"
+echo "Ram: I am doing good. What are you doing Sam?"
+echo "Sam: Currently I'm learning Shell Scripting"
