ba76b4ab957f5e1f52bc2d97ce394ea5c52f1255
TarakPalati <tarakpalati@gmail.com>
2024-09-10 17:18:10 +0530

    Updated the Script

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,4 +3,4 @@
 echo "Ram: Hi Sam, how are you"
 echo "Sam: Hi Ram, I am doing good. How about you Ram?"
 echo "Ram: I am doing good. What are you doing Sam?"
-echo "Sam: Currently I'm learning Shell Scripting"
+echo "Sam: Currently I'm learning Shell Scripting, Sam"
